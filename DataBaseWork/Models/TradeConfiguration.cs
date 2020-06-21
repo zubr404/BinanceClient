68aa619ebfa46bffc395cabf682856b67f1b8820
zubr404 <sss635@yandex.ru>
2020-06-22 00:42:50 +0300

    Добавлены/изменены модели Strategy, TradeConfiguration, User

---
+++ DataBaseWork/Models/TradeConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBaseWork.Models
+{
+    public class TradeConfiguration
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string MainCoin { get; set; }
+        public string AltCoin { get; set; }
+        public int StrategyID { get; set; }
+        public int IntervalHttp { get; set; }
+        public int OpenOrders { get; set; }
+        public double OrderIndent { get; set; }
+        public double OrderDeposit { get; set; }
+        public double OrderStep { get; set; }
+        public double OrderStepPlus { get; set; }
+        public double Martingale { get; set; }
+        public double DepositLimit { get; set; }
+        public double OrderReload { get; set; }
+        public double StopLoss { get; set; }
+        public double Profit { get; set; }
+        public double Tralling { get; set; }
+        public bool Active { get; set; }
+        public Strategy Strategy { get; set; }
+    }
+}
--- DataBaseWork/Models/User.cs
+++ DataBaseWork/Models/User.cs
@@ -10,10 +10,14 @@ namespace DataBaseWork.Models
         public string Login { get; set; }
         public string Password { get; set; }
         public List<APIKey> APIKeys { get; set; }
+        public List<Trade> Trades { get; set; }
+        public List<TradeConfiguration> TradeConfigurations { get; set; }
 
         public User()
         {
             APIKeys = new List<APIKey>();
+            Trades = new List<Trade>();
+            TradeConfigurations = new List<TradeConfiguration>();
         }
     }
 }
