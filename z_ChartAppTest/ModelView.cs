a0afb8b5a212b84b92b04ffafa1443e6d82616ce
zubr404 <sss635@yandex.ru>
2021-03-16 01:49:26 +0300

    Chart - часть 2

--- z_ChartAppTest/ModelView.cs
+++ z_ChartAppTest/ModelView.cs
@@ -1,16 +1,63 @@
-﻿using System;
+﻿using Services;
+using StockExchenge.Charts;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using z_ChartAppTest.Models;
 using z_ChartAppTest.Services;
 
 namespace z_ChartAppTest
 {
     class ModelView
     {
-        public Candlestick Candlestick { get; private set; }
+        public double GridHeight { get; private set; }
+        public double GridWidth { get; private set; }
+        public CandlestickService CandlestickService { get; private set; }
+
+        private Kline kline;
+        private List<Candle> candles;
         public ModelView()
         {
-            Candlestick = new Candlestick();
+            GridHeight = 800;
+            GridWidth = 1150;
+            CandlestickService = new CandlestickService();
+
+            kline = new Kline("BTCUSDT", "5m");
+            candles = new List<Candle>();
+            ChartStart();
+        }
+
+        private void ChartStart()
+        {
+            GetHistoryCandle();
+            CandlestickService.GetMaxAllChart(candles);
+            CandlestickService.GetMinAllChart(candles);
+            CandlestickService.GetDeltaAllChart(candles);
+            CandlestickService.GetScaleIntervalPrice(GridHeight);
+            CandlestickService.CreateChart(candles);
+        }
+
+        private void GetHistoryCandle()
+        {
+            try
+            {
+                var klineString = kline.GetHistory();
+                var klines = JConverter.JsonConver<List<object[]>>(klineString);
+
+                foreach (var k in klines)
+                {
+                    var ohlcPoint = new Candle(Convert.ToDouble(k[2], new CultureInfo("en-US")),
+                        Convert.ToDouble(k[3], new CultureInfo("en-US")),
+                        Convert.ToDouble(k[1], new CultureInfo("en-US")),
+                        Convert.ToDouble(k[4], new CultureInfo("en-US")));
+                    candles.Add(ohlcPoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                // запись логов в БД
+            }
         }
     }
 }
---
+++ z_ChartAppTest/Models/Candle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace z_ChartAppTest.Models
+{
+    public class Candle
+    {
+        public double High { get { return high; } }
+        private double high;
+
+        public double Low { get { return low; } }
+        private double low;
+
+        public double Open { get { return open; } }
+        private double open;
+
+        public double Close { get { return close; } }
+        private double close;
+
+        public bool IsPositive { get { return ispositive; } }
+        private bool ispositive;
+
+        public Candle(double high, double low, double open, double close)
+        {
+            this.high = high;
+            this.low = low;
+            this.open = open;
+            this.close = close;
+            SetIsPositive();
+        }
+
+        private void SetIsPositive()
+        {
+            if(Close > Open)
+            {
+                ispositive = true;
+            }
+            else
+            {
+                ispositive = false;
+            }
+        }
+    }
+}
